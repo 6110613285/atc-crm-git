5fab569c90be0797df49a00dab307ad19c438dbb
praewa ainsuwan <praewa12800@gmail.com>
2025-05-20 16:29:35 +0700

    ปุ่มลอย

---
+++ src/Stockpage/bucket.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FormControl, Button, Table, Tabs, Tab, Card, Badge, Container, Alert } from "react-bootstrap";
+function HelloComponent() {
+  return <h1>Hello</h1>;
+}
+
+export default HelloComponent;
